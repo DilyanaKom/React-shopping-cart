2e86eb667323263bc729658fa7f9396252281d57
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-06-06 14:01:50 +0300

    Create and style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
 import './Header.css';
+import { Link, NavLink } from 'react-router';
+import Logo from '../assets/u4198439476_online_shop_logo_--ar_43_--v_6.1_976adb77-ee41-4320-b157-0ff6d3cc1b6e_1.png'
 interface Props {}
 
 function Header(props: Props) {
     const {} = props
 
     return (
-        <div>Header</div>
+        <header>
+            <Link to="/" className="logo">
+            <img src={Logo}/>
+            <span>Shopping Cart</span>
+            </Link>
+            <nav className="navigation">
+                <NavLink to="/" className="link">Home</NavLink>
+                <NavLink to="/cart" className="link">Cart</NavLink>
+
+            </nav>
+            <Link to="/cart" className="items">
+            <span>Cart: </span>
+            </Link>
+        </header>
     )
 }
 
